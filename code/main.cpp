c52100273163681cf5e4f7480840af3c66ddca1b
Justin-Fernbaugh <fernbaughj@gmail.com>
2021-09-08 16:05:07 -0700

    Added SQLite3 database code.

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,36 @@
+#include <stdio.h>
 #include <iostream>
+#include <sqlite3.h>
 
 
 using namespace std; 
 
 int main() { 
-	cout << "test " << endl;
+	int input, deposit, withdrawl;
+
+
+	cout << "##############" << endl;
+	cout << " Banking SQL" << endl;
+	cout << "##############" << endl;
+
+	cout << "1. Desposit 2. Withdrawl ";
+	cin >> input;
+	cout << "input chosen: " << input << endl;
+
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	rc = sqlite3_open("/../SQL/database.db", &db);
+
+	if( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return(0);
+	} else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	sqlite3_close(db);
+
+
 	return 0;
 }
